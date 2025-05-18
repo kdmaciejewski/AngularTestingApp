de1413a62ac905b9f86e56f19df7252a44b8fb61
Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>
2025-05-18 18:47:50 +0200

    Added hydration and ssr

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideClientHydration, withIncrementalHydration, withEventReplay } from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideClientHydration(withIncrementalHydration()), provideClientHydration(withEventReplay()),
+  ],
+}).catch(err => console.error(err));
+
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideRouter(routes),
+//     provideHttpClient(),
+//     provideClientHydration(),
+//   ],
+// }).catch(err => console.error(err));
